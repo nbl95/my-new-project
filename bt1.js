9f12497c9a10c1142f971595d870aae575004cda
nbl95 <nbl.ltu12@gmail.com>
2019-11-27 14:06:18 +0700

    baitap1

---
+++ bt1.js
@@ -0,0 +1,8 @@
+const x = Number(prompt("Vật Lí"));
+const y = Number(prompt("Hóa Học"));
+const z = Number(prompt("Sinh Học"));
+const b=(x+y+z)/3;
+const a = (x+y+z);
+
+document.getElementById('tong').innerHTML= a ;
+document.getElementById('trungbinh').innerHTML= b ;
